372fe3cdc4822c66c7eb065448294e714bc8a9aa
zack-bitcoin <zack.bitcoin@gmail.com>
2019-12-08 01:20:26 -0300

    made it more readable

--- vdf_calculate.c
+++ vdf_calculate.c
@@ -6,37 +6,38 @@
 
 int main(int argc, char *argv[])
 {
-  char * hash = argv[1];
-  char * pos;
-  BYTE hash2[32];
-
-  for (size_t count = 0; count < sizeof hash2/sizeof *hash2; count++) {
-    sscanf(pos, "%2hhx", &hash2[count]);
-    pos += 2;
-  }
-
+  char * inhex = argv[1];
   int many = atoi(argv[2]);
   int period = atoi(argv[3]);
-  BYTE buf[32];
+
+  BYTE hash[32];
+  SHA256_CTX ctx;//stores intermediate state while computing a hash.
+  BYTE buf[32];//stores intermediate state between computing hashes.
+
+  //convert the hex input into bytes.
+  for (size_t count = 0; count < sizeof hash/sizeof *hash; count++) {
+    sscanf(inhex, "%2hhx", &hash[count]);
+    inhex += 2;
+  }
 
   for (int i = 0; i < many; i++)
+    //we do 'many' rounds of checkpoints
     {
       for(int j = 0; j < period; j++)
+        //for every checkpoint, the number of hashes we compute is 'period'
         {
-          //update hash2 to be sha256(hash2);
-          SHA256_CTX ctx;
+          //update hash to be sha256(hash);
           sha256_init(&ctx);
-          sha256_update(&ctx, hash2, 32);
-          //BYTE buf[32];
+          sha256_update(&ctx, hash, 32);
           sha256_final(&ctx, buf);
           for(int z = 0; z < 32; z++)
             {
-              hash2[z] = buf[z];
+              hash[z] = buf[z];
             }
         }
       //store a checkpoint
       printf("checkpoint\n");
-      printf("%s", hash2);
+      printf("%s", hash);
       printf("\n");
     }
 }
